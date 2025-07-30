39d6ebcd4b0d635b6f6f315bf66aa33caa3682eb
Stepan Zdrazil <stopka.zdrazil@gmail.com>
2025-07-30 06:09:49 +0200

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/cosmic-carbon-quest">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
